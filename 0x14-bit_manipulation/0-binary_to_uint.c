735bd15565a6aa4bc07359c8d5397c34f98cfa36
abdulmujeeb29 <abdulmujeebtiamiyu@gmail.com>
2023-05-05 08:31:43 -0700

    start

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,18 +3,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, num=0 , field ;
+	unsigned int i, num = 0, field;
+
 	if (!b)
 		return (num);
 
-	for (i = strlen(b) - 1, field =1; (signed int)i >= 0; i-- ,field *= 2)
-		if (b[i] =='1')
-			num +=field;
-	        else if (b[i] =='0')
+	for (i = strlen(b) - 1, field = 1; (signed int)i >=0; field *=2)
+		if (b[i] == '1')
+			num += field;
+	        else if (b[i] == '0')
 			continue;
-	        else 
+	        else
 			return (0);
 	return (num);
-
 }
 
