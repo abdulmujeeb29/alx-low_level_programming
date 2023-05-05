c5ee7c661637384753633d149ae431f332a5a648
Tiamiyu Abdulmujeeb Olamilekan <110623915+abdulmujeeb29@users.noreply.github.com>
2023-05-05 20:43:07 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,3 +1,9 @@
+/**
+* get_bit - get a bi value of @n at a given @index
+* @n: the number that has the bit 
+* @index: the index of the bit 
+*
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
