4a886e51d411a6bb6ed277e0417fc43e23f7a94c
abdulmujeeb29 <abdulmujeebtiamiyu@gmail.com>
2023-05-11 15:40:28 -0700

    new commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,14 +11,14 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len =0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0;, text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
